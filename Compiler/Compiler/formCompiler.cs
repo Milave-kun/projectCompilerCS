69136c4bf4d908ba7e1c2177455e0b34a98cb845
mfulo <142382665+mfulo@users.noreply.github.com>
2023-12-14 22:37:21 +0800

    Add files via upload

--- Compiler/Compiler/formCompiler.cs
+++ Compiler/Compiler/formCompiler.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.IO;
 using System.Windows.Forms;
 
@@ -10,6 +12,19 @@ namespace Compiler
         {
             InitializeComponent();
         }
+
+       // List<string> lines = new();
+        List<string> lexemes = new();
+        List<string> tokens = new();
+        List<string> dataTypes = new List<string>
+        {
+            "int","float","boolean","char","String"
+        };
+        List<string> fixedTokens = new List<string>
+        {
+            "<data_type>","<identifier>","<assignment_operator>","<value>","<delimiter>"
+        };
+        string lines = "";
 
 // Button Open File
         private void btnOpenFile_Click_1(object sender, EventArgs e)
@@ -24,13 +39,33 @@ namespace Compiler
             {
                 string filePath = openFileDialog.FileName;
 
-                // Enable Buttons or perform other action here
-
-                // Read the contents of the file
                 try
                 {
-                    string fileContents = File.ReadAllText(filePath);
-                    txtCodeTextArea.Text = fileContents;
+                    //string fileContents = File.ReadAllText(filePath);
+                    //txtCodeTextArea.Text = fileContents;
+
+                    string lines = File.ReadAllText(filePath);
+                    txtCodeTextArea.Text = lines;
+
+                    // Read all lines from the file and store them in a List<string>
+                    //lines = ReadLinesFromFile(filePath);
+
+
+                    //List<string> ReadLinesFromFile(string filePath)
+                    //{
+                    //    List<string> liness = new List<string>();
+
+                    //    // Read all lines from the file and add them to the List
+                    //    using (StreamReader reader = new StreamReader(filePath))
+                    //    {
+                    //        while (!reader.EndOfStream)
+                    //        {
+                    //            string line = reader.ReadLine();
+                    //            liness.Add(line);
+                    //        }
+                    //    }
+                    //    return liness;
+                    //}
 
                     // Enable lexical analysis button
                     btnLexicalAnalysis.Enabled = true;
@@ -38,21 +73,25 @@ namespace Compiler
                     btnClear.Enabled = true;
                     // Disable Open File button
                     btnOpenFile.Enabled = false;
-                }
+
+                 
+                } 
+                
                 catch (Exception ex)
                 {
                     // Handle file reading errors
                     MessageBox.Show("Error reading the file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
+
             else
             {
                 // Disable buttons or perform other actions here
                 btnLexicalAnalysis.Enabled = false;
                 btnClear.Enabled = false;
                 btnOpenFile.Enabled = true; // Enable Open File button if the dialog is canceled
-            }
-        }
+            }  
+        } // openFile
 
 // Button Clear Text
         private void btnClear_Click(object sender, EventArgs e)
@@ -80,36 +119,131 @@ namespace Compiler
 // Button Lexical Analysis
         private void btnLexicalAnalysis_Click_1(object sender, EventArgs e)
         {
+            
+            string result = "";
             // Perform lexical analysis here and display results in a message box
 
-            // For example, you can check a condition and set the result
-            string result = CheckLexicalAnalysis(txtCodeTextArea.Text);
+            // take each line to split them to lexemes
+            //foreach (string line in lines)
+            //{
+              
+                    bool isPass = true;
+
+                    // split each word by space
+                    // store to array
+                    // convert array to list
+                    string[] lexemeArray = lines.Split(' ');
+                   lexemes = new List<string>(lexemeArray);
+
+                    foreach (string lexeme in lexemes)
+                    {
+                        string token = getToken(lexeme);
+                        tokens.Add(token);
+
+                    }
+
+                    foreach (string token in tokens)
+                    {
+                        if (!fixedTokens.Contains(token))
+                        {
+                            isPass = false;
+                            break;
+                        }
+
+                    
+                    }
+
+                    ////clear Lists for the next line
+                    //lexemes.Clear();
+                    //tokens.Clear();
+
+                //if (!isPass)
+                //{
+                //    // No need to continue checking lines if isPass is false
+                //    break;
+                //}
+            //}
+
+           
+
+            // update result
+            if (result.Equals("Pass"))
+            {
+               
+                btnSyntaxAnalysis.Enabled = true;
+            }
+               
 
             // Display a message box based on the analysis result
-            MessageBox.Show(result == "Accepted" ? "Lexical analysis passed!" : "Lexical analysis failed!", "Analysis Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show(result == "Pass" ? "Lexical analysis passed!" : "Lexical analysis failed!", "Analysis Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
             // Disable the lexical analysis button after displaying result
             btnLexicalAnalysis.Enabled = false;
 
-            // Sample lexical analysis function
+         
         }
 
-        // Sample lexical analysis function
-        private string CheckLexicalAnalysis(string code)
-        {
-            // Implement your lexical analysis logic here
-            // This is a placeholder, you need to replace it with your actual logic
+       
 
-            // For example, check a condition and return "Accepted" or "Not Accepted"
-            if (code.Contains("some condition"))
+        // my sample function
+        // findToken
+        private string getToken (string lexeme)
+        {
+            // use var for type inferencing
+            var token = ""; 
+
+            // check if lexeme is equal to any of the data types
+            if (dataTypes.Contains(lexeme))
+                token = "<data_type>";
+            
+            else if (lexeme.Equals("="))
+                token = "<assignment_operator>";
+            
+            else if (lexeme.Equals(";"))
+                token = "<delimiter>";
+            
+            else if (isValidVar(lexeme))
+                token = "<identifier>";
+            
+            else if (isValue(lexeme))
+                token = "<value>";
+            
+
+            return token;
+        }
+
+        private bool isValidVar (string lexeme)
+        {
+            if (dataTypes.Contains(lexeme))
+                return false;
+
+            //if (!char.IsLetter(lexeme[0]))
+            //    return false;
+
+            return char.IsLetter(lexeme[0]);
+        }
+
+        private bool isValue (string lexeme)
+        {
+            if (lexeme.StartsWith("\"") && lexeme.EndsWith("\""))
+                return true;
+
+            if (lexeme.StartsWith("\'") && lexeme.EndsWith("\'"))
             {
-                btnSyntaxAnalysis.Enabled = true;
-                return "Accepted";
+                if (lexeme.Length == 3 && char.IsLetter(lexeme[1]))
+                    // dataType = "char";
+                    return true;
+                else
+                    // dataType = "String";
+                    return true;
             }
-            else
-            {
-                return "Not Accepted";
-            }
+            
+
+            if (int.TryParse(lexeme, out _))
+                return true;
+
+            
+            return false;
         }
 
 // Button Syntax Analysis
